ecc232e6b96689348595c3385d34afde45ea9f64
ROBIN_SACHER <110891559+robinsacher@users.noreply.github.com>
2022-11-08 09:55:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace LA_1200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string inPath = @"List.txt";
+            string text = File.ReadAllText(inPath);
+            string[] lines = text.Split(";");
+            int mode;
+            int i = 0;
+
+            Console.WriteLine("Welcome to this Vacabluary learning Programm!");
+            
+
+            for (i = 0; i < lines.Length; i++)
+            {
+                string[] words = lines[i].Split(";");
+                Console.Write(words[0]);
+            }
+
+
+
+        }
+    }
+}
